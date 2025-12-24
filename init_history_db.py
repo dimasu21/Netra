11cbacbeac3e2571facb99d51608aa258af45a3d
Dimas Tri M <dimsartz021@gmail.com>
2025-12-24 12:26:00 +0700

    big update

--- init_history_db.py
+++
@@ -1,8 +0,0 @@
-
-from app import app
-from models import db
-
-with app.app_context():
-    print("Creating all tables (safe mode - only new tables created)...")
-    db.create_all()
-    print("Database tables updated.")
